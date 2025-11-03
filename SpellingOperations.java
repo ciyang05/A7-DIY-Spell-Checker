ac06439a1db0ace87cf507c8ab1e09e2773cd459
kiaras-create <ksunilsingh@smith.edu>
2025-11-02 21:10:01 -0500

    Inserting and deleting letters

--- SpellingOperations.java
+++ SpellingOperations.java
@@ -16,6 +16,33 @@ public interface SpellingOperations {
    *  @param query the word to check
    *  @return a list of all valid words that are one edit away from the query
    */
-  public ArrayList<String> nearMisses(String query);
-  
+  public ArrayList<String> nearMisses(String query){
+  ArrayList<String> suggestions = new ArrayList<>();
+    // Inserting one Letter
+    for (int i = 0; i<= query.lenght(); i++){
+      for (char c ='a'; c <='z'; c++ ){
+        String newWord = query.substring(0, i) + c + query.substring(i);
+        if (containsWord(newWord)){
+          suggestions.add(newWord);
+        }
+
+    }
+  }
+  //Removing letters
+  for (int i = 0; i < query.length(); i++){
+    StringBuilder sb = new StringBuilder(query);
+    String newWord = sb.deleteCharAt(i);
+    if (containsWord(newWord)){
+      suggestions.add(newWord);
+    }
+  }
+
+  //
+  return suggestions;
+
+  }
+
 }
+ 
+
+  
