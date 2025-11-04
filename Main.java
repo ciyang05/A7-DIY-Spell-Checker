d143ab63be431b09baaf772c274af34bb2f9bb75
Shakila Jafari <shakilajafari@Shakilas-MacBook-Air.local>
2025-11-03 23:12:10 -0500

    Shakila

--- Main.java
+++ Main.java
@@ -2,6 +2,18 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
+        WordValidation validator = new WordValidation("words.txt");
+
+        testContainsWord(validator);
+        testNearMisses_Deletion(validator);
+        testNearMisses_Insertion(validator);
+        testNearMisses_Substitution(validator);
+        testNearMisses_Transposition(validator);
+        testNearMisses_Split(validator);
+        
+    }
+
+    private static void testContainsWord(WordValidation validator) {
         
     }
     
